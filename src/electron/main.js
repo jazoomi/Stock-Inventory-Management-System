2670bcbff21a1abb64985a10f8af9469004c447d
Nickrod Basiri <basirinickrod@gmail.com>
2025-02-13 17:04:14 -0500

    Express server start/stop

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,8 +1,34 @@
-import {app, BrowserWindow} from "electron";
+import { app, BrowserWindow } from "electron";
 import path from "path";
+import { spawn } from "child_process";
 
-app.on("ready", ()=> {
-    const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
-})
+let serverProcess; 
+
+app.on("ready", () => {
+    // Start Express 
+    serverProcess = spawn("node", ["backend/server.js"], {
+        detached: true,
+        stdio: "ignore",
+    });
+
+    serverProcess.unref(); 
+
+    console.log("Express server started");
+
+    // Open Electron window
+    const mainWindow = new BrowserWindow({
+        disableBlinkFeatures: "Autofill",
+    });
+
+    // Load frontend
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+});
+
+// Kill Express when Electron exits
+app.on("quit", () => {
+    if (serverProcess) {
+        serverProcess.kill();
+        console.log("Express server closed");
+    }
+});
 
